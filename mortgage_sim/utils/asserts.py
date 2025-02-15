f60b0e451cb614217384c694a3a453f823000e98
fafnir <gokurohono@gmail.com>
2025-02-16 00:47:50 +1100

    chore: update asserttype

--- mortgage_sim/utils/asserts.py
+++ mortgage_sim/utils/asserts.py
@@ -1,24 +1,21 @@
-
-def assert_type(input: any, expected_types: type | tuple[type,...]) -> None:
+def assert_type(input: any, expected_types: type | tuple[type, ...]) -> None:
     """essentially is instance check wrapper."""
-    
+
     if not isinstance(expected_types, (type, tuple)):
-        raise ValueError("expected_types expected type or tuple[types,...]",
-                         f"instead got {type(expected_types)}")
+        raise ValueError(
+            "expected_types expected type or tuple[types,...]",
+            f"instead got {type(expected_types)}",
+        )
 
     if type(expected_types) is tuple:
-        _expected = list(lambda x: type(x), expected_types)
         for val in expected_types:
             if not isinstance(val, type):
-                raise ValueError("expected Types in the provided tuple"
-                                 f"instead got: {_expected}")
+                raise ValueError(
+                    f"expected Types in the provided tupleinstead got: {expected_types}"
+                )
 
     if not isinstance(input, expected_types):
-        _expected = expected_types
-        if isinstance(_expected, tuple):
-            _expected = list(lambda x: type(x), expected_types)
         raise TypeError(
-            "Type Assertion failed:"
-            f"Expected: {_expected}\n"
-            f"Instead got: {type(input)}"
+            f"Type Assertion failed:Expected: {expected_types}\n",
+            "Instead got: {type(input)}",
         )
