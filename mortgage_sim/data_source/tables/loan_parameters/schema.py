4ddb1559765fdd05688ed76ee1dbc12e07694da1
fafnir <gokurohono@gmail.com>
2025-02-15 16:17:47 +1100

    feat: adding loan parameter table schema

--- mortgage_sim/data_source/tables/loan_parameters/schema.py
+++ mortgage_sim/data_source/tables/loan_parameters/schema.py
@@ -7,4 +7,8 @@ class LoanParametersTableSchema(SchemaTemplate):
     def get_polars_schema(self) -> dict[str, pl.DataType]:
         return {
             "uuid": pl.String(),
+            "principle_amount": pl.Float32(),
+            "interest_rate": pl.Float32(),  # range(0,100) i.e. not a decimal
+            "monthly_repayments": pl.Float32(),
+            "update_date": pl.Date(),
         }
