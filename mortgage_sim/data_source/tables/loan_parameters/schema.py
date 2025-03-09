f3fbfb9ab6929738c39d4fe6648d53b7d76ac818
fafnir <gokurohono@gmail.com>
2025-03-09 13:51:08 +1100

    feat: update schema for loan parameters

--- mortgage_sim/data_source/tables/loan_parameters/schema.py
+++ mortgage_sim/data_source/tables/loan_parameters/schema.py
@@ -8,16 +8,26 @@ class LoanParametersTableSchema(SchemaTemplate):
     def get_polars_schema(self) -> dict[str, pl.DataType]:
         return {
             "uuid": pl.String(),
-            "principle_amount": pl.Float32(),
             "interest_rate": pl.Float32(),  # range(0,100) i.e. not a decimal
             "monthly_repayment": pl.Float32(),
+            "parameters_change_start_date": pl.Date(),
+            # these params below should never change.
+            "principle_amount": pl.Float32(),
+            "monthly_repayment_date": pl.Date(),
+            "monthly_interest_charge_date": pl.Date(),
+            "loan_start_date": pl.Date(),
         }
 
     @classmethod
     def get_python_schema(self) -> dict[str, pl.DataType]:
         return {
             "uuid": str,
-            "principle_amount": float,
             "interest_rate": (float, type(None)),  # range(0,100) i.e. not a decimal
             "monthly_repayment": (float, type(None)),
+            "parameters_change_start_date": date,
+            # these params below should never change.
+            "principle_amount": float,
+            "monthly_repayment_date": date,  # NOTE this should never change
+            "monthly_interest_charge_date": date,  # NOTE this should never change
+            "loan_start_date": date,  # NOTE this should never change
         }
